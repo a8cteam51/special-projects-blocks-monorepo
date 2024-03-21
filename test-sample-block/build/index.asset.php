cb91d2def276994d2251239ef5d326b8cbc12ed3
mlaetitia <30905719+mlaetitia@users.noreply.github.com>
2024-03-21 16:40:49 -0500

    test branch - testing how to add a new block

---
+++ test-sample-block/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'a76169ffa3fb8bf94e2d');
---
+++ test-sample-block/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'ec69a644d56030355287');
