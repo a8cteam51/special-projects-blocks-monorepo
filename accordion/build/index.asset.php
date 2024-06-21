ecf287cc54588ddeb66cd5a3d33c184f25088474
Jeff Ong <jonger4@gmail.com>
2024-06-21 13:33:26 -0400

    Use background as mask instead.

--- accordion/build/index.asset.php
+++ accordion/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '4cdd7e192952ce18785f');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '49af58d48b57f35229cd');
