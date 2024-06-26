d912eac82d3182e9bd5ed9d8af287bf60d49fa78
Jeff Ong <jonger4@gmail.com>
2024-06-26 17:21:06 -0400

    Disable line breaks.

--- stretchy-type/build/index.asset.php
+++ stretchy-type/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '12619497b8ec078fe9cc');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => 'cbadbe26870aa5e7a709');
