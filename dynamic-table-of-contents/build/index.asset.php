544fa4baa81d6c5084bc5aa122a6d7888e04b1f4
Chris Bell <chrisbcodeable@gmail.com>
2024-07-07 18:52:28 +0100

    fix: missing block icon

--- dynamic-table-of-contents/build/index.asset.php
+++ dynamic-table-of-contents/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '3ace790a30b183b13771');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'aabbec29f3f59c974851');
--- dynamic-table-of-contents/src/index.js
+++ dynamic-table-of-contents/src/index.js
@@ -20,6 +20,23 @@ import './style.scss';
 import Edit from './edit';
 import metadata from './block.json';
 
+const icon = (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 24 24"
+		width="24"
+		height="24"
+		aria-hidden="true"
+		focusable="false"
+	>
+		<path
+			fill-rule="evenodd"
+			clip-rule="evenodd"
+			d="M20 9.484h-8.889v-1.5H20v1.5Zm0 7h-4.889v-1.5H20v1.5Zm-14 .032a1 1 0 1 0 0-2 1 1 0 0 0 0 2Zm0 1a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z"
+		></path>
+		<path d="M13 15.516a2 2 0 1 1-4 0 2 2 0 0 1 4 0ZM8 8.484a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"></path>
+	</svg>
+);
 /**
  * Every block starts by registering a new block type definition.
  *
@@ -30,4 +47,6 @@ registerBlockType( metadata.name, {
 	 * @see ./edit.js
 	 */
 	edit: Edit,
+
+	icon,
 } );
