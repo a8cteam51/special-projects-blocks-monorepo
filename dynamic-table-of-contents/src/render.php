5927f2fc49d9bd56e4192f24f7e5fe8774ef6360
Chris Bell <chrisbcodeable@gmail.com>
2024-07-07 18:51:28 +0100

    feat: add filter to block title output

--- dynamic-table-of-contents/src/render.php
+++ dynamic-table-of-contents/src/render.php
@@ -1,5 +1,7 @@
 <?php
 /**
+ * Render the block on the frontend.
+ *
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
 
@@ -20,9 +22,12 @@ wp_enqueue_script(
 );
 ?>
 
-<div <?php echo get_block_wrapper_attributes(); ?>>
+<div <?php echo wp_kses_post( get_block_wrapper_attributes() ); ?>>
 	<?php
-	if ( isset( $attributes['title'] ) && ! empty( $attributes['title'] ) ) {
+	$block_title = isset( $attributes['title'] ) ?? '';
+	$block_title = apply_filters( 'wpsp_dynamic_table_of_contents_block_title', $block_title, $attributes );
+
+	if ( ! empty( $block_title ) ) {
 		printf(
 			'<h2>%s</h2>',
 			wp_kses_post( $attributes['title'] )
