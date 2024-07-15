470c42f77256019294a922cca47c0fb9b89ea679
TommusRhodus <tom.rhodes@automattic.com>
2024-07-15 11:23:38 +0100

    Mass update of plugin update URLs

--- hello-world/hello-world.php
+++ hello-world/hello-world.php
@@ -8,7 +8,7 @@
  * Author:            The WordPress Contributors
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Update URI:        https://opsoasis-develop.mystagingwebsite.com/hello-world/
+ * Update URI:        https://opsoasis.wpspecialprojects.com/hello-world/
  * Text Domain:       hello-world
  *
  * @package           wpsp
