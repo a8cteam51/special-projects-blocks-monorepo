2104bba5804e4623c84b55e4b93ba8003e6e8360
Michal Czaplinski <mmczaplinski@gmail.com>
2024-07-17 19:18:56 +0100

    chore: Add save function to marquee block

--- marquee/src/index.js
+++ marquee/src/index.js
@@ -6,8 +6,10 @@ import "./style.scss";
  * Internal dependencies
  */
 import Edit from "./edit";
+import save from "./save";
 import metadata from "./block.json";
 
 registerBlockType(metadata.name, {
 	edit: Edit,
+	save,
 });
