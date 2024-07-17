2f7ca579267ec4620e71457bb770f978b2e81e4a
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2024-07-17 10:54:06 +0200

    Add option to add new tabs from the sidebar

--- tabs/src/tabs/edit.js
+++ tabs/src/tabs/edit.js
@@ -3,16 +3,27 @@
  */
 import {
 	BlockIcon,
+	InspectorControls,
 	store as blockEditorStore,
 	useBlockDisplayInformation,
 	useBlockProps,
 	useInnerBlocksProps,
 } from '@wordpress/block-editor';
-import { createBlocksFromInnerBlocksTemplate } from '@wordpress/blocks';
-import { Button, Placeholder, TextControl } from '@wordpress/components';
+import {
+	createBlock,
+	createBlocksFromInnerBlocksTemplate,
+} from '@wordpress/blocks';
+import {
+	Button,
+	PanelBody,
+	PanelRow,
+	Placeholder,
+	TextControl,
+} from '@wordpress/components';
 import { useDispatch, useSelect } from '@wordpress/data';
 import { useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
+import { create } from '@wordpress/icons';
 
 /**
  * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
@@ -36,7 +47,42 @@ function createInnerTabsTemplate( count ) {
 	return template;
 }
 
-function TabsEdit( { attributes: { templateLock } } ) {
+function TabsInspectorControls( { clientId } ) {
+	const tabs = useSelect(
+		( select ) => {
+			return select( blockEditorStore ).getBlocks( clientId );
+		},
+		[ clientId ]
+	);
+	const { title, icon } = useBlockDisplayInformation( tabs[ 0 ].clientId );
+	const { insertBlock } = useDispatch( blockEditorStore );
+
+	function addNewTab() {
+		insertBlock( createBlock( TAB_BLOCK_NAME ), tabs.length, clientId );
+	}
+
+	return (
+		<InspectorControls>
+			<PanelBody title={ __( 'Tabs', 'tabs' ) }>
+				{ tabs.map( ( tab ) => (
+					<PanelRow key={ tab.clientId }>
+						<BlockIcon icon={ icon } showColors />
+						{ title }
+					</PanelRow>
+				) ) }
+				<PanelRow>
+					<Button
+						icon={ create }
+						label={ __( 'Add a new tab', 'tabs' ) }
+						onClick={ addNewTab }
+					/>
+				</PanelRow>
+			</PanelBody>
+		</InspectorControls>
+	);
+}
+
+function TabsEdit( { attributes: { templateLock }, clientId } ) {
 	const blockProps = useBlockProps();
 	const innerBlocksProps = useInnerBlocksProps( blockProps, {
 		defaultBlock: TAB_BLOCK,
@@ -46,7 +92,12 @@ function TabsEdit( { attributes: { templateLock } } ) {
 		templateLock,
 	} );
 
-	return <div { ...innerBlocksProps } />;
+	return (
+		<>
+			<TabsInspectorControls clientId={ clientId } />
+			<div { ...innerBlocksProps } />
+		</>
+	);
 }
 
 function TabsPlaceholder( { clientId } ) {
