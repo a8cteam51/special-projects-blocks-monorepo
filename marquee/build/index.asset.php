12ec8296d5a905ded5c49a7ee748eb43c7dd335d
Michal Czaplinski <mmczaplinski@gmail.com>
2024-07-17 18:45:50 +0100

    remove the build files

--- marquee/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => 'c8334640ad7b9b1b92c6');
--- marquee/build/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => 'd4187ecd923728b96fa5', 'type' => 'module');
