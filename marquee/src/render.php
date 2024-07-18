5966a734d6c086ff712392fc779fe0e914f4a21d
Michal Czaplinski <mmczaplinski@gmail.com>
2024-07-18 13:28:45 +0100

    Refactor marquee block rendering and cloning logic

--- marquee/src/render.php
+++ marquee/src/render.php
@@ -1,16 +1,16 @@
 <?php
-	$inner_blocks = $block->inner_blocks;
+	$inner_blocks      = $block->inner_blocks;
 	$inner_blocks_html = '';
-	foreach ($inner_blocks as $inner_block) {
-    $inner_blocks_html .= $inner_block->render();
-	}
+foreach ( $inner_blocks as $inner_block ) {
+	$inner_blocks_html .= $inner_block->render();
+}
 ?>
 
 <div
 	data-wp-interactive="wpcomsp/marquee"
 	data-wp-init="callbacks.init"
 >
-	<div <?php echo get_block_wrapper_attributes(array( "class" => 'wp-block-wpcomsp-marquee__inner')); ?>>
+	<div <?php echo get_block_wrapper_attributes( array( 'class' => 'wp-block-wpcomsp-marquee__inner' ) ); ?>>
 		<?php echo $inner_blocks_html; ?>
 	</div>
 </div>
--- marquee/src/view.js
+++ marquee/src/view.js
@@ -4,8 +4,25 @@ store("wpcomsp/marquee", {
 	callbacks: {
 		init: () => {
 			const { ref } = getElement();
-			const clonedElement = ref.firstElementChild.cloneNode(true);
-			ref.appendChild(clonedElement);
+
+			// Get all the children of the marquee element.
+			const children = ref.querySelectorAll(".wp-block-wpcomsp-marquee__inner");
+
+			// Measure the width of the marquee element.
+			const width = ref.getBoundingClientRect().width;
+
+			// Measure the width of all the children.
+			let childrenWidth = 0;
+
+			// If the width of the children is less than the width of the marquee
+			// element, clone the children to fill the marquee element.
+			while (childrenWidth <= width) {
+				children.forEach((child) => {
+					const clone = child.cloneNode(true);
+					ref.appendChild(clone);
+					childrenWidth += child.getBoundingClientRect().width;
+				});
+			}
 		},
 	},
 });
