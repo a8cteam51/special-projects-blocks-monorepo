3a72b2b8c8f0bc59661e6a9ead1a0d1175b35761
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2024-07-19 13:26:02 +0200

    Reuse the example code for tabs accesible behavior Source: https://www.w3.org/WAI/ARIA/apg/patterns/tabs/examples/tabs-automatic/.

--- tabs/src/tabs/view.js
+++ tabs/src/tabs/view.js
@@ -1,25 +1,138 @@
-/**
- * Use this file for JavaScript code that you want to run in the front-end
- * on posts/pages that contain this block.
+/*
+ *   This content is licensed according to the W3C Software License at
+ *   https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document
  *
- * When this file is defined as the value of the `viewScript` property
- * in `block.json` it will be enqueued on the front end of the site.
+ *   File:   tabs-automatic.js
  *
- * Example:
- *
- * ```js
- * {
- *   "viewScript": "file:./view.js"
- * }
- * ```
- *
- * If you're not making any changes to this file because your project doesn't need any
- * JavaScript running in the front-end, then you should delete this file and remove
- * the `viewScript` property from `block.json`.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-metadata/#view-script
+ *   Desc:   Tablist widget that implements ARIA Authoring Practices
  */
 
-/* eslint-disable no-console */
-console.log( 'Hello World! (from wpcomsp-tabs block)' );
-/* eslint-enable no-console */
+'use strict';
+
+class TabsAutomatic {
+	constructor( groupNode ) {
+		this.tablistNode = groupNode;
+
+		this.tabs = [];
+
+		this.firstTab = null;
+		this.lastTab = null;
+
+		this.tabs = Array.from(
+			this.tablistNode.querySelectorAll( '[role=tab]' )
+		);
+		this.tabpanels = [];
+
+		for ( let i = 0; i < this.tabs.length; i += 1 ) {
+			const tab = this.tabs[ i ];
+			const tabpanel = document.getElementById(
+				tab.getAttribute( 'aria-controls' )
+			);
+
+			tab.tabIndex = -1;
+			this.tabpanels.push( tabpanel );
+
+			tab.addEventListener( 'keydown', this.onKeydown.bind( this ) );
+			tab.addEventListener( 'click', this.onClick.bind( this ) );
+
+			if ( ! this.firstTab ) {
+				this.firstTab = tab;
+			}
+			this.lastTab = tab;
+		}
+	}
+
+	setSelectedTab( currentTab, setFocus ) {
+		if ( typeof setFocus !== 'boolean' ) {
+			setFocus = true;
+		}
+		for ( let i = 0; i < this.tabs.length; i += 1 ) {
+			const tab = this.tabs[ i ];
+			if ( currentTab === tab ) {
+				tab.setAttribute( 'aria-selected', 'true' );
+				tab.removeAttribute( 'tabindex' );
+				this.tabpanels[ i ].removeAttribute( 'hidden' );
+				if ( setFocus ) {
+					tab.focus();
+				}
+			} else {
+				tab.setAttribute( 'aria-selected', 'false' );
+				tab.tabIndex = -1;
+				this.tabpanels[ i ].setAttribute( 'hidden', true );
+			}
+		}
+	}
+
+	setSelectedToPreviousTab( currentTab ) {
+		let index;
+
+		if ( currentTab === this.firstTab ) {
+			this.setSelectedTab( this.lastTab );
+		} else {
+			index = this.tabs.indexOf( currentTab );
+			this.setSelectedTab( this.tabs[ index - 1 ] );
+		}
+	}
+
+	setSelectedToNextTab( currentTab ) {
+		let index;
+
+		if ( currentTab === this.lastTab ) {
+			this.setSelectedTab( this.firstTab );
+		} else {
+			index = this.tabs.indexOf( currentTab );
+			this.setSelectedTab( this.tabs[ index + 1 ] );
+		}
+	}
+
+	/* EVENT HANDLERS */
+
+	onKeydown( event ) {
+		const tgt = event.currentTarget;
+		let flag = false;
+
+		switch ( event.key ) {
+			case 'ArrowLeft':
+				this.setSelectedToPreviousTab( tgt );
+				flag = true;
+				break;
+
+			case 'ArrowRight':
+				this.setSelectedToNextTab( tgt );
+				flag = true;
+				break;
+
+			case 'Home':
+				this.setSelectedTab( this.firstTab );
+				flag = true;
+				break;
+
+			case 'End':
+				this.setSelectedTab( this.lastTab );
+				flag = true;
+				break;
+
+			default:
+				break;
+		}
+
+		if ( flag ) {
+			event.stopPropagation();
+			event.preventDefault();
+		}
+	}
+
+	onClick( event ) {
+		this.setSelectedTab( event.currentTarget );
+	}
+}
+
+// Initialize tablists.
+window.addEventListener( 'load', function () {
+	const tablists = document.querySelectorAll(
+		'.wp-block-wpcomsp-tabs [role=tablist]'
+	);
+	for ( let i = 0; i < tablists.length; i++ ) {
+		new TabsAutomatic( tablists[ i ] );
+	}
+} );
