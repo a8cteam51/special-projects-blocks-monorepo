8d2821ad1ecc382a45e89b8e443ea905d8181b51
Jeff Ong <jonger4@gmail.com>
2024-07-24 16:08:07 -0400

    Use wrapping div so padding can be applied as expected.

--- accordion/src/accordion-content/edit.js
+++ accordion/src/accordion-content/edit.js
@@ -1,20 +1,59 @@
 import { __ } from '@wordpress/i18n';
-import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
+import {
+	InnerBlocks,
+	useBlockProps,
+	__experimentalUseBorderProps as useBorderProps,
+	__experimentalUseColorProps as useColorProps,
+	__experimentalGetSpacingClassesAndStyles as useSpacingProps,
+	__experimentalGetShadowClassesAndStyles as useShadowProps,
+} from '@wordpress/block-editor';
+import clsx from 'clsx';
 
-export default function Edit( { attributes: { templateLock } } ) {
+export default function Edit( { attributes } ) {
+	const { allowedBlocks, templateLock } = attributes;
 	const blockProps = useBlockProps();
+	const borderProps = useBorderProps( attributes );
+	const colorProps = useColorProps( attributes );
+	const spacingProps = useSpacingProps( attributes );
+	const shadowProps = useShadowProps( attributes );
 
 	return (
-		<div { ...blockProps }>
-			<InnerBlocks
-				template={ [
-					[
-						'core/paragraph',
-						{ placeholder: 'Accordion item content goes here.' },
-					],
-				] }
-				templateLock={ false }
-			/>
+		<div
+			{ ...blockProps }
+			className={ clsx(
+				blockProps.className,
+				colorProps.className,
+				borderProps.className,
+				{
+					[ `has-custom-font-size` ]: blockProps?.style?.fontSize,
+				}
+			) }
+			style={ {
+				...borderProps.style,
+				...colorProps.style,
+				...shadowProps.style,
+			} }
+		>
+			<div
+				className="wpsp-accordion-content__wrapper"
+				style={ {
+					...spacingProps.style,
+				} }
+			>
+				<InnerBlocks
+					allowedBlocks={ allowedBlocks }
+					template={ [
+						[
+							'core/paragraph',
+							{
+								placeholder:
+									'Accordion item content goes here.',
+							},
+						],
+					] }
+					templateLock={ templateLock }
+				/>
+			</div>
 		</div>
 	);
 }
--- accordion/src/accordion-content/save.js
+++ accordion/src/accordion-content/save.js
@@ -1,10 +1,46 @@
 import { __ } from '@wordpress/i18n';
-import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
+import {
+	InnerBlocks,
+	useBlockProps,
+	__experimentalGetBorderClassesAndStyles as getBorderClassesAndStyles,
+	__experimentalGetColorClassesAndStyles as getColorClassesAndStyles,
+	__experimentalGetSpacingClassesAndStyles as getSpacingClassesAndStyles,
+	__experimentalGetShadowClassesAndStyles as getShadowClassesAndStyles,
+} from '@wordpress/block-editor';
+import clsx from 'clsx';
 
 export default function save( { attributes } ) {
+	const blockProps = useBlockProps.save();
+	const borderProps = getBorderClassesAndStyles( attributes );
+	const colorProps = getColorClassesAndStyles( attributes );
+	const spacingProps = getSpacingClassesAndStyles( attributes );
+	const shadowProps = getShadowClassesAndStyles( attributes );
+
 	return (
-		<div { ...useBlockProps.save() }>
-			<InnerBlocks.Content />
+		<div
+			{ ...blockProps }
+			className={ clsx(
+				blockProps.className,
+				colorProps.className,
+				borderProps.className,
+				{
+					[ `has-custom-font-size` ]: blockProps?.style?.fontSize,
+				}
+			) }
+			style={ {
+				...borderProps.style,
+				...colorProps.style,
+				...shadowProps.style,
+			} }
+		>
+			<div
+				className="wpsp-accordion-content__wrapper"
+				style={ {
+					...spacingProps.style,
+				} }
+			>
+				<InnerBlocks.Content />
+			</div>
 		</div>
 	);
 }
