20ad058d14c0a8b6e8f6e03b04ed2051211a7325
Jeff Ong <jonger4@gmail.com>
2024-07-26 17:12:36 -0400

    Bump minor version.
    Bump version.

--- accordion/accordion.php
+++ accordion/accordion.php
@@ -4,7 +4,7 @@
  * Description:       Container block for accordion items.
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           0.2.0
+ * Version:           0.3.0
  * Author:            Studio 51
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
