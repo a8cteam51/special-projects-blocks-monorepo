35821662209a34aa333040d1cf3c297d933bac80
David Arenas <david.arenas@automattic.com>
2024-10-23 11:43:34 +0200

    Fix fontSize prop

--- stretchy-paragraph/src/view.js
+++ stretchy-paragraph/src/view.js
@@ -9,6 +9,7 @@ store("wpcomsp/stretchy-paragraph", {
 			tempElement.style.position = "absolute";
 			tempElement.style.whiteSpace = "nowrap";
 			tempElement.style.visibility = "hidden";
+			tempElement.style.fontSize = "200px";
 
 			tempElement.innerHTML = ref.innerHTML;
 			ref.appendChild(tempElement);
@@ -20,7 +21,7 @@ store("wpcomsp/stretchy-paragraph", {
 
 			tempElement.remove();
 
-			ref.style.fontSize = `calc( ${fontSize / textWidth} * 100cqw );`;
+			ref.style.fontSize = `calc( ${fontSize / textWidth} * 100cqw )`;
 			ref.parentElement.style.containerType = "inline-size";
 		},
 	},
