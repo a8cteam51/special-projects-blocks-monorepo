b4d4605153d8f6afc9bec1f453fceb6545456bd0
David Arenas <david.arenas@automattic.com>
2024-10-25 01:55:59 +0200

    Rename dimensions attr to viewBox

--- stretchy-type/build/index.asset.php
+++ stretchy-type/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '647609ea5af52688ca49');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'a89f602685b4ad57ac8d');
