3c2b29b186f05967d5e78633457fc6b4b3f2f875
Mario Santos <santosguillamot@gmail.com>
2024-10-25 08:38:57 +0200

    Remove duplicated style

--- stretchy-type/src/edit.js
+++ stretchy-type/src/edit.js
@@ -1,62 +1,45 @@
-import { RichText, useBlockProps } from '@wordpress/block-editor';
-import { useRef, useEffect } from '@wordpress/element';
+import { RichText, useBlockProps } from "@wordpress/block-editor";
+import { useRef, useEffect } from "@wordpress/element";
 
-export default function Edit( { attributes, setAttributes } ) {
+export default function Edit({ attributes, setAttributes }) {
 	const { content, viewBox } = attributes;
-	const blockProps = useBlockProps( viewBox ? { viewBox } : {} );
+	const blockProps = useBlockProps(viewBox ? { viewBox } : {});
 	const wrapperRef = useRef();
 	const richTextRef = useRef();
 
-	useEffect( () => {
-		const observer = new window.ResizeObserver( () => {
+	useEffect(() => {
+		const observer = new window.ResizeObserver(() => {
 			const { offsetWidth, offsetHeight } = wrapperRef.current;
-			setAttributes( {
-				viewBox: `0 0 ${ offsetWidth } ${ offsetHeight }`,
-			} );
+			setAttributes({
+				viewBox: `0 0 ${offsetWidth} ${offsetHeight}`,
+			});
 			// This hack is required to prevent RichText to overwrite `white-space`.
-			richTextRef.current.style.whiteSpace = 'nowrap';
-		} );
-		observer.observe( wrapperRef.current );
+			richTextRef.current.style.whiteSpace = "nowrap";
+		});
+		observer.observe(wrapperRef.current);
 
 		return () => {
 			observer.disconnect();
 		};
-	}, [] );
+	}, []);
 
-	const onChange = ( nextContent ) => {
-		setAttributes( { content: nextContent } );
+	const onChange = (nextContent) => {
+		setAttributes({ content: nextContent });
 	};
 
 	return (
-		<svg
-			{ ...blockProps }
-			style={ {
-				display: 'inline-block',
-				width: '100%',
-				fontFamily: 'inherit',
-				maxWidth: 'none !important',
-			} }
-		>
+		<svg {...blockProps}>
 			<foreignObject x="0" y="0" width="100%" height="100%">
-				<span
-					ref={ wrapperRef }
-					style={ {
-						fontSize: '1em',
-						whiteSpace: 'nowrap',
-						display: 'inline-block',
-						width: 'fit-content',
-						height: 'fit-content',
-					} }
-				>
+				<span ref={wrapperRef}>
 					<RichText
 						disableLineBreaks
 						tagName="span"
 						identifier="content"
 						placeholder="Stretchy text goes here"
 						preserveWhiteSpace
-						value={ content }
-						onChange={ onChange }
-						ref={ richTextRef }
+						value={content}
+						onChange={onChange}
+						ref={richTextRef}
 					/>
 				</span>
 			</foreignObject>
