f00bb60fcf1cf02a0f9d4b4478dc162b2ada3074
Phil Cable <phil@happyprime.co>
2025-04-16 13:16:30 -0700

    Add PHP rendering
    It's a bummer to have to replicate all this, but it affords the ability to change things without invalidating saved blocks.

--- carousel/carousel.php
+++ carousel/carousel.php
@@ -10,7 +10,7 @@
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       carousel
  *
- * @package Wpcomsp
+ * @package wpcomsp
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -45,14 +45,7 @@ add_filter(
 	}
 );
 
-/**
- * Registers the block using the metadata loaded from the `block.json` file.
- * Behind the scenes, it registers also all assets so they can be enqueued
- * through the block editor in the corresponding context.
- *
- * @see https://developer.wordpress.org/reference/functions/register_block_type/
- */
-function wpcomsp_carousel_block_init() {
-	register_block_type( __DIR__ . '/build' );
+// Autoload all files in the includes directory.
+foreach ( glob( __DIR__ . '/includes/*.php' ) as $wpcomsp_filename ) {
+	include $wpcomsp_filename;
 }
-add_action( 'init', 'wpcomsp_carousel_block_init' );
