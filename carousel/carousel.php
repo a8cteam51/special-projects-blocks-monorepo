bc96b25691b067b15b96b052f4d797efa721325a
jeremyfelt <jeremy.felt@gmail.com>
2025-04-21 11:36:10 -0700

    Don't check wp.org for updates to this plugin slug

--- carousel/carousel.php
+++ carousel/carousel.php
@@ -9,6 +9,7 @@
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       carousel
+ * Update URI:        false
  *
  * @package wpcomsp
  */
