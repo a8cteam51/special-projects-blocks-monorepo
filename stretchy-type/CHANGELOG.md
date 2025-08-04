8a0759155f4182de0934bb990a61c7b93416843e
TommusRhodus <tom.rhodes@automattic.com>
2025-08-04 14:26:14 +0100

    Stretchy Type: Version bump

--- stretchy-type/CHANGELOG.md
+++ stretchy-type/CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.1.2  (2025-08-02)
+
+- Fixed front-end class not applying.
+
 ## 0.1.1 (2025-08-02)
 
 ### Added
--- stretchy-type/stretchy-type.php
+++ stretchy-type/stretchy-type.php
@@ -4,7 +4,7 @@
  * Description:       A block that expands to fill the width of its container.
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           0.1.1
+ * Version:           0.1.2
  * Author:            Studio 51
  * Author URI:        https://wpspecialprojects.wordpress.com/
  * Update URI:        https://opsoasis.wpspecialprojects.com/stretchy-type/
