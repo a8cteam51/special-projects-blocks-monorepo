bc3341439863f90ce058641e4fac150ebb3bacbe
James Kenneth Guidaven <jkguidaven@gmail.com>
2025-08-08 20:38:39 +0800

    Show tab title in the InspectorControls

--- tabs/src/tabs/edit.js
+++ tabs/src/tabs/edit.js
@@ -72,6 +72,7 @@ function TabsInspectorControls( { clientId, setAttributes } ) {
 			<PanelBody title={ __( 'Tabs', 'tabs' ) }>
 				{ tabs.map( ( tab, index ) => {
 					const tabNumber = index + 1;
+					const title = tab.attributes.title || `Tab ${ tabNumber }`;
 					return (
 						<PanelRow key={ tab.clientId }>
 							<Button
@@ -80,7 +81,7 @@ function TabsInspectorControls( { clientId, setAttributes } ) {
 									activeTab: tabNumber,
 								} ) }
 							>
-								{ `Tab ${ tabNumber }` }
+								{ title }
 							</Button>
 						</PanelRow>
 					);
