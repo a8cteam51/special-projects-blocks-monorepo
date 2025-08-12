1f034c067bc1580784134df1abf4e731e99aae74
James Kenneth Guidaven <jkguidaven@gmail.com>
2025-08-12 21:39:29 +0800

    Remove unnecessary xmls.

--- tabs/src/tab/index.js
+++ tabs/src/tab/index.js
@@ -21,13 +21,7 @@ import save from './save';
 import metadata from './block.json';
 
 const TabIcon = (
-	<SVG
-		width="24"
-		height="24"
-		viewBox="0 0 24 24"
-		fill="none"
-		xmlns="http://www.w3.org/2000/svg"
-	>
+	<SVG width="24" height="24" viewBox="0 0 24 24" fill="none">
 		<Path
 			fillRule="evenodd"
 			clipRule="evenodd"
