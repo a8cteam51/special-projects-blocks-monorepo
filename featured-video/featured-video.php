cd7e8810661b3658b2208062094dcd2537f1f697
Tremi Dkhar <tremi@dkhar.com>
2025-08-21 13:06:41 +0530

    Bump Featured Video Version

--- featured-video/featured-video.php
+++ featured-video/featured-video.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Featured Video
  * Description: Add the ability to use Featured Video inplace of Featured Image.
- * Version: 0.1.0
+ * Version: 0.1.1
  * Author: The WordPress Contributors
  * License: GPL-2.0-or-later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
