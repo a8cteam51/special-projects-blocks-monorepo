312ddc5f051114e96c28674411d050a2c886f836
TommusRhodus <tom.rhodes@automattic.com>
2025-08-21 20:01:30 +0100

    Adds missing update URI

--- flowing-column/flowing-column.php
+++ flowing-column/flowing-column.php
@@ -9,6 +9,7 @@
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       flowing-column
+ * Update URI:        https://opsoasis.wpspecialprojects.com
  *
  * @package A8csp
  */
