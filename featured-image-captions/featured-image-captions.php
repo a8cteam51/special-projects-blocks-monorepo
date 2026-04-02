a03211c1a9c4878b0cb3d8c4dd1494c7ce943124
James Shakespeare <j@jshakespeare.com>
2026-04-02 12:02:02 +0100

    add featured image captions plugin from Earth Species Project

---
+++ featured-image-captions/featured-image-captions.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * Plugin Name:       Featured Image Captions
+ * Description:       Adds caption support to the core/post-featured-image block.
+ * Requires at least: 6.1
+ * Requires PHP:      8.0
+ * Version:           1.0.0
+ * Author:            Automattic Special Projects Team
+ * Author URI:        https://wpspecialprojects.wordpress.com/
+ * Update URI:        https://opsoasis.wpspecialprojects.com/featured-image-captions/
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       featured-image-captions
+ *
+ * @package wpcomsp
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+// If no other WPCOMSP Block Plugin added the self update class, add it.
+if ( ! class_exists( 'WPCOMSP_Blocks_Self_Update' ) ) {
+	require __DIR__ . '/classes/class-wpcomsp-blocks-self-update.php';
+
+	WPCOMSP_Blocks_Self_Update::get_instance()->hooks();
+}
+
+/**
+ * Enqueues the editor script for the Featured Image caption extension.
+ *
+ * @return void
+ */
+function featured_image_captions_editor_assets(): void {
+	$asset_file = __DIR__ . '/build/index.asset.php';
+
+	if ( ! file_exists( $asset_file ) ) {
+		return;
+	}
+
+	$asset_meta = require $asset_file;
+
+	wp_enqueue_script(
+		'featured-image-captions-editor',
+		plugins_url( 'build/index.js', __FILE__ ),
+		$asset_meta['dependencies'],
+		$asset_meta['version'],
+		true
+	);
+}
+add_action( 'enqueue_block_editor_assets', 'featured_image_captions_editor_assets' );
+
+/**
+ * Enqueues the frontend styles for the Featured Image caption extension.
+ *
+ * @return void
+ */
+function featured_image_captions_frontend_styles(): void {
+	$asset_file = __DIR__ . '/build/index.asset.php';
+
+	if ( ! file_exists( $asset_file ) ) {
+		return;
+	}
+
+	$asset_meta = require $asset_file;
+
+	wp_enqueue_style(
+		'featured-image-captions',
+		plugins_url( 'build/index.css', __FILE__ ),
+		array(),
+		$asset_meta['version']
+	);
+}
+add_action( 'wp_enqueue_scripts', 'featured_image_captions_frontend_styles' );
+
+/**
+ * Appends a caption to the Featured Image block output.
+ *
+ * @param string               $block_content The block content.
+ * @param array<string, mixed> $block         The block data.
+ *
+ * @return string
+ */
+function featured_image_captions_render_caption( string $block_content, array $block ): string {
+	if ( empty( $block['attrs']['isCaptionEnabled'] ) ) {
+		return $block_content;
+	}
+
+	$figcaption = '';
+	if ( ! empty( $block['attrs']['caption'] ) && is_string( $block['attrs']['caption'] ) ) {
+		$figcaption = $block['attrs']['caption'];
+	} else {
+		$figcaption = get_the_post_thumbnail_caption();
+	}
+
+	if ( is_string( $figcaption ) && '' !== $figcaption ) {
+		$block_content = str_replace(
+			'</figure>',
+			'<figcaption class="wp-element-caption">' . wp_kses_post( $figcaption ) . '</figcaption></figure>',
+			$block_content
+		);
+	}
+
+	return $block_content;
+}
+add_filter( 'render_block_core/post-featured-image', 'featured_image_captions_render_caption', 10, 2 );
+
+/**
+ * Setup auto-updates for this plugin from our monorepo.
+ *
+ * @param array $blocks Array of plugin slugs.
+ *
+ * @return array
+ */
+add_filter(
+	'wpcomsp_installed_blocks',
+	function ( $blocks ) {
+		$blocks[] = 'featured-image-captions';
+		return $blocks;
+	}
+);
---
+++ featured-image-captions/src/index.js
@@ -0,0 +1,158 @@
+// WordPress dependencies.
+import { addFilter } from '@wordpress/hooks';
+import { createHigherOrderComponent } from '@wordpress/compose';
+import { __ } from '@wordpress/i18n';
+import { BlockControls, RichText } from '@wordpress/block-editor';
+import { ToolbarGroup, ToolbarButton } from '@wordpress/components';
+
+import './index.css';
+
+/**
+ * Extends the settings of the 'core/post-featured-image' block by adding
+ * additional attributes for caption functionality.
+ *
+ * @param {Object} settings - The current settings of the block.
+ * @param {string} name     - The name of the block.
+ * @return {Object} The extended settings of the block.
+ */
+function extendFeaturedImage( settings, name ) {
+	if ( 'core/post-featured-image' === name ) {
+		return Object.assign( {}, settings, {
+			attributes: Object.assign( {}, settings.attributes, {
+				isCaptionEnabled: { type: 'boolean', default: false },
+				caption: { type: 'string', default: '' },
+			} ),
+		} );
+	}
+
+	return settings;
+}
+
+addFilter(
+	'blocks.registerBlockType',
+	'team51-featured-image-captions/attributes',
+	extendFeaturedImage
+);
+
+/**
+ * Higher-order component that adds a toolbar button to the 'core/post-featured-image' block
+ * to toggle the caption functionality.
+ *
+ * @param {Function} BlockEdit - The original block edit component.
+ * @return {Function} The enhanced block edit component.
+ */
+const withToolbarFeaturedImage = createHigherOrderComponent( ( BlockEdit ) => {
+	return ( props ) => {
+		// If current block is not allowed
+		if ( 'core/post-featured-image' !== props.name ) {
+			return <BlockEdit { ...props } />;
+		}
+
+		const { attributes, setAttributes } = props;
+		const { isCaptionEnabled } = attributes;
+
+		const icon = () => {
+			return (
+				<svg
+					viewBox="0 0 24 24"
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					aria-hidden="true"
+					focusable="false"
+				>
+					<path
+						fillRule="evenodd"
+						clipRule="evenodd"
+						d="M6 5.5h12a.5.5 0 0 1 .5.5v12a.5.5 0 0 1-.5.5H6a.5.5 0 0 1-.5-.5V6a.5.5 0 0 1 .5-.5ZM4 6a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6Zm4 10h2v-1.5H8V16Zm5 0h-2v-1.5h2V16Zm1 0h2v-1.5h-2V16Z"
+					></path>
+				</svg>
+			);
+		};
+
+		return (
+			<div>
+				<BlockControls group="block">
+					<ToolbarGroup>
+						<ToolbarButton
+							icon={ icon }
+							label={
+								isCaptionEnabled
+									? __(
+											'Remove Caption',
+											'featured-image-captions'
+									  )
+									: __(
+											'Add Caption',
+											'featured-image-captions'
+									  )
+							}
+							isActive={ isCaptionEnabled }
+							onClick={ () => {
+								setAttributes( {
+									isCaptionEnabled: ! isCaptionEnabled,
+								} );
+							} }
+						/>
+					</ToolbarGroup>
+				</BlockControls>
+				<BlockEdit { ...props } />
+			</div>
+		);
+	};
+}, 'withCaptionFeaturedImage' );
+
+addFilter(
+	'editor.BlockEdit',
+	'featured-image-captions/toolbar',
+	withToolbarFeaturedImage
+);
+
+/**
+ * Higher-order component that adds a caption input field to the 'core/post-featured-image' block
+ * if the caption functionality is enabled.
+ *
+ * @param {Function} BlockListBlock - The original block list block component.
+ * @return {Function} The enhanced block list block component.
+ */
+const withCaptionFeaturedImage = createHigherOrderComponent(
+	( BlockListBlock ) => {
+		return ( props ) => {
+			// If current block is not allowed
+			if ( 'core/post-featured-image' !== props.name ) {
+				return <BlockListBlock { ...props } />;
+			}
+
+			const { attributes, setAttributes } = props;
+			const { isCaptionEnabled, caption } = attributes;
+
+			if ( ! isCaptionEnabled ) {
+				return <BlockListBlock { ...props } />;
+			}
+
+			return (
+				<div>
+					<BlockListBlock { ...props } />
+					<RichText
+						tagName="figcaption"
+						identifier="caption"
+						label={ __( 'Caption', 'featured-image-captions' ) }
+						value={ caption }
+						onChange={ ( value ) =>
+							setAttributes( { caption: value } )
+						}
+						inlineToolbar
+						placeholder="Add caption (Leave blank to use image caption)"
+					/>
+				</div>
+			);
+		};
+	},
+	'withCaptionFeaturedImage'
+);
+
+addFilter(
+	'editor.BlockListBlock',
+	'featured-image-captions/caption',
+	withCaptionFeaturedImage
+);
