b300afd035ea40174fc6a79283ee7617c3e722fc
Belinda <belinda@verytwisty.com>
2026-07-16 17:28:05 +0100

    Updates to the groups contributors block

--- bp-groups/src/bp-groups-contributors/edit.js
+++ bp-groups/src/bp-groups-contributors/edit.js
@@ -17,7 +17,12 @@ import {
 	InspectorControls,
 } from '@wordpress/block-editor';
 
-import { TextControl, RangeControl, PanelBody } from '@wordpress/components';
+import {
+	TextControl,
+	RangeControl,
+	PanelBody,
+	ToggleControl,
+} from '@wordpress/components';
 
 /**
  * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
@@ -40,43 +45,92 @@ import './editor.scss';
  * @return {Element} Element to render.
  */
 export default function Edit( { attributes, setAttributes } ) {
-	const { perPage, countText, allMembersText, avatarSize } = attributes;
+	const {
+		perPage,
+		countText,
+		allMembersText,
+		avatarSize,
+		showAvatars,
+		showMemberCount,
+		memberLink,
+	} = attributes;
+
 	const avatars = perPage ? [ ...Array( perPage ).keys() ] : [];
 
 	return (
 		<>
 			<InspectorControls>
 				<PanelBody title={ __( 'Settings', 'bp-groups-blocks' ) }>
-					<TextControl
-						label={ __(
-							'Contributors per page',
-							'bp-groups-blocks'
-						) }
-						type="number"
-						value={ perPage }
+					<h3>{ __( 'Avatar Settings', 'bp-groups-blocks' ) }</h3>
+					<ToggleControl
+						label={ __( 'Show avatars', 'bp-groups-blocks' ) }
+						checked={ showAvatars }
 						onChange={ ( value ) =>
-							setAttributes( { perPage: parseInt( value ) } )
+							setAttributes( { showAvatars: value } )
 						}
-						help={ __(
-							'Number of contributors to display before the show more button appears.',
-							'bp-groups-blocks'
-						) }
 					/>
-					<RangeControl
-						label={ __( 'Avatar Size', 'bp-groups-blocks' ) }
-						value={ avatarSize }
+					{ showAvatars && (
+						<>
+							<TextControl
+								label={ __(
+									'Contributors per page',
+									'bp-groups-blocks'
+								) }
+								type="number"
+								value={ perPage }
+								onChange={ ( value ) =>
+									setAttributes( {
+										perPage: parseInt( value ),
+									} )
+								}
+								help={ __(
+									'Number of contributors to display before the show more button appears.',
+									'bp-groups-blocks'
+								) }
+							/>
+							<RangeControl
+								label={ __(
+									'Avatar Size',
+									'bp-groups-blocks'
+								) }
+								value={ avatarSize }
+								onChange={ ( value ) =>
+									setAttributes( { avatarSize: value } )
+								}
+								min={ 10 }
+								max={ 150 }
+							/>
+							<ToggleControl
+								label={ __(
+									'Show member link',
+									'bp-groups-blocks'
+								) }
+								checked={ memberLink }
+								onChange={ ( value ) =>
+									setAttributes( { memberLink: value } )
+								}
+								help={ __(
+									'If enabled, clicking on a contributor avatar will take the user to their profile page.',
+									'bp-groups-blocks'
+								) }
+							/>
+						</>
+					) }
+					<h3>{ __( 'Member Count', 'bp-groups-blocks' ) }</h3>
+					<ToggleControl
+						label={ __( 'Show member count', 'bp-groups-blocks' ) }
+						checked={ showMemberCount }
 						onChange={ ( value ) =>
-							setAttributes( { avatarSize: value } )
+							setAttributes( { showMemberCount: value } )
 						}
-						min={ 10 }
-						max={ 150 }
 					/>
 				</PanelBody>
 			</InspectorControls>
 			<div { ...useBlockProps() }>
 				<p>
-					{ `${ perPage ? perPage * 2 : 0 }` }
-					&nbsp;
+					{ `${
+						perPage && showMemberCount ? perPage * 2 + ' ' : ''
+					}` }
 					<RichText
 						tagName="span"
 						value={ countText }
@@ -88,33 +142,36 @@ export default function Edit( { attributes, setAttributes } ) {
 							'bp-groups-blocks'
 						) }
 					/>
-					&nbsp;
-					<RichText
-						tagName="button"
-						value={ allMembersText }
-						onChange={ ( value ) =>
-							setAttributes( { allMembersText: value } )
-						}
-						placeholder={ __(
-							'All members text',
-							'bp-groups-blocks'
-						) }
-						className="bp-groups-contributors__view-all"
-					/>
+					{ showAvatars && (
+						<RichText
+							tagName="button"
+							value={ allMembersText }
+							onChange={ ( value ) =>
+								setAttributes( { allMembersText: value } )
+							}
+							placeholder={ __(
+								'All members text',
+								'bp-groups-blocks'
+							) }
+							className="bp-groups-contributors__view-all"
+						/>
+					) }
 				</p>
-				<ul className="bp-groups-contributors__avatars">
-					{ avatars.map( ( _, index ) => (
-						<li
-							key={ index }
-							className="bp-groups-contributors__avatar"
-							style={ {
-								backgroundColor: '#ccc',
-								width: avatarSize,
-								height: avatarSize,
-							} }
-						></li>
-					) ) }
-				</ul>
+				{ showAvatars && (
+					<ul className="bp-groups-contributors__avatars">
+						{ avatars.map( ( _, index ) => (
+							<li
+								key={ index }
+								className="bp-groups-contributors__avatar"
+								style={ {
+									backgroundColor: '#ccc',
+									width: avatarSize,
+									height: avatarSize,
+								} }
+							></li>
+						) ) }
+					</ul>
+				) }
 			</div>
 		</>
 	);
--- bp-groups/src/bp-groups-contributors/render.php
+++ bp-groups/src/bp-groups-contributors/render.php
@@ -20,51 +20,66 @@ if ( false === $members ) {
 	return '';
 }
 
-$members_count    = $members['count'];
-$members_per_page = $attributes['perPage'] ?? 5;
-$count_text       = $attributes['countText'] ?? __( 'members', 'bp-groups-blocks' );
-$all_members_text = $attributes['allMembersText'] ?? __( 'View All', 'bp-groups-blocks' );
-$size             = $attributes['avatarSize'] ?? 50;
-$img_size         = $size < 50 ? 'thumb' : 'full';
-$i                = 1;
+$members_count     = $members['count'];
+$show_avatars      = $attributes['showAvatars'] ?? true;
+$show_member_count = $attributes['showMemberCount'] ?? true;
+$member_link       = $attributes['memberLink'] ?? true;
+$members_per_page  = $attributes['perPage'] ?? 5;
+$count_text        = $attributes['countText'] ?? __( 'members', 'bp-groups-blocks' );
+$all_members_text  = $attributes['allMembersText'] ?? __( 'View All', 'bp-groups-blocks' );
+$size              = $attributes['avatarSize'] ?? 50;
+$img_size          = $size < 50 ? 'thumb' : 'full';
+$i                 = 1;
 ?>
 
 <div <?php echo wp_kses_data( get_block_wrapper_attributes() ); ?>>
 	<?php
 	printf(
-		'<p class="bp-groups-contributors__count">%d %s %s</p>',
-		intval( $members_count ),
+		'<p class="bp-groups-contributors__count">%s %s %s</p>',
+		$show_member_count ? intval( $members_count ) : '',
 		esc_html( $count_text ),
-		$members_count > $members_per_page ? '<button class="bp-groups-contributors__view-all" tabindex="0">' . esc_html( $all_members_text ) . '</button>' : ''
+		$show_avatars && ( $members_count > $members_per_page ) ? '<button class="bp-groups-contributors__view-all" tabindex="0">' . esc_html( $all_members_text ) . '</button>' : ''
 	);
 	?>
 
-	<ul class="bp-groups-contributors__avatars">
-		<?php
-		if ( bp_group_has_members( $args ) ) {
-			while ( bp_group_members() ) {
-				bp_group_the_member();
-				?>
-					<li
-						class="bp-groups-contributors__avatar"
-						<?php echo $i > $members_per_page ? 'hidden' : ''; ?>
-					>
-						<?php
-						echo wp_kses_post(
-							bp_get_group_member_avatar(
-								array(
-									'type'   => $img_size,
-									'width'  => $size,
-									'height' => $size,
-								)
-							)
-						);
-						?>
-					</li>
-				<?php
-				++$i;
+	<?php if ( $show_avatars ) { ?>
+		<ul class="bp-groups-contributors__avatars">
+			<?php
+			if ( bp_group_has_members( $args ) ) {
+				while ( bp_group_members() ) {
+					bp_group_the_member();
+
+					$member_avatar = bp_get_group_member_avatar(
+						array(
+							'type'   => $img_size,
+							'width'  => $size,
+							'height' => $size,
+						)
+					);
+
+					$member_link = $member_link ? bp_get_group_member_domain() : '';
+					?>
+						<li
+							class="bp-groups-contributors__avatar"
+							<?php echo $i > $members_per_page ? 'hidden' : ''; ?>
+						>
+							<?php
+							if ( $member_link ) {
+								printf(
+									'<a href="%s" class="bp-groups-contributors__avatar-link">%s</a>',
+									esc_url( $member_link ),
+									wp_kses_post( $member_avatar )
+								);
+							} else {
+								echo wp_kses_post( $member_avatar );
+							}
+							?>
+						</li>
+					<?php
+					++$i;
+				}
 			}
-		}
-		?>
-	</ul>
+			?>
+		</ul>
+	<?php } ?>
 </div>
